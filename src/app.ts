71cdca4d05b3fe275f53bbe07e164cf70d6540a7
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-02-05 22:29:43 +0100

    ProductController and more routes.

--- src/app.ts
+++ src/app.ts
@@ -9,19 +9,25 @@ dotenvFlow.config();
 // create express application
 const app: Application = express();
 
-app.use('/api', routes);
+
 
 /**
  * 
  */
 export function startServer() {
 
+    // JSON body parser
+    app.use(express.json());
+
+    // bind routes to the app
+    app.use('/api', routes);
+
     // test the connection to the database
     testConnection();
 
     // start the server
     const PORT: number = parseInt(process.env.PORT as string) || 4000;
-    app.listen(PORT, function() {
+    app.listen(PORT, function () {
         console.log("Server is up and running on port: " + PORT);
     });
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,11 @@
 import { Router, Request, Response } from 'express';
+import {
+    createProduct,
+    getAllProducts,
+    getProductById,
+    updateProductById,
+    deleteProductById
+} from './controllers/productController';
 
 const router: Router = Router();
 
@@ -6,9 +13,15 @@ const router: Router = Router();
  * 
  */
 router.get('/', (req: Request, res: Response) => {
-    // connect
     res.status(200).send('Welcome to the MENTS API');
-    // disconnect
 });
+
+
+router.post('/products', createProduct);
+router.get('/products', getAllProducts);
+router.get('/products/:id', getProductById);
+router.put('/products/:id', updateProductById);
+router.delete('/products/:id', deleteProductById);
+
 
 export default router;
