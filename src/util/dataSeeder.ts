c95b0155cffd0675c643b322dcf784b632086c11
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-03-10 16:04:58 +0100

    Improved data seeder using Faker

--- src/util/dataSeeder.ts
+++ src/util/dataSeeder.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import dotenvFlow from "dotenv-flow";
+import { faker } from '@faker-js/faker';
 
 // Project import
 import { productModel } from "../models/productModel";
@@ -46,71 +47,33 @@ export async function seedData() {
   const passwordHash = await bcrypt.hash("12345678", salt);
 
   const user1 = new userModel();
-  user1.name = "Peter Petersen";
-  user1.email = "peter@petersen.com";
+  user1.name = faker.person.fullName();
+  user1.email = faker.internet.email();
   user1.password = passwordHash;
   await user1.save();
 
   const user2 = new userModel();
-  user2.name = "Heidi Jensen";
-  user2.email = "heidi@jensen.com";
+  user2.name = faker.person.fullName();
+  user2.email = faker.internet.email();
   user2.password = passwordHash;
   await user2.save();
 
-  const user3 = new userModel();
-  user3.name = "Mr Burns";
-  user3.email = "mr@burns.com";
-  user3.password = passwordHash;
-  await user3.save();
-
-  const products = [
-    {
-      name: "Product #1 (made by Peter)",
-      description: "Product #1 description",
-      imageURL: "https://picsum.photos/500/500",
-      price: 2,
-      stock: 20,
-      discount: true,
-      discountPct: 10,
-      isHidden: false,
-      _createdBy: user1.id,
-    },
-    {
-      name: "Product #2 (made by Peter)",
-      description: "Product #2 description",
-      imageURL: "https://picsum.photos/500/500",
-      price: 100.96,
-      stock: 0,
-      discount: true,
-      discountPct: 25,
-      isHidden: false,
-      _createdBy: user1.id,
-    },
-    {
-      name: "Product #3 (made by Heidi)",
-      description: "Product #3 description",
-      imageURL: "https://picsum.photos/500/500",
-      price: 192.96,
-      stock: 150,
-      discount: false,
-      discountPct: 0,
-      isHidden: false,
-      _createdBy: user2.id,
-    },
-    {
-      name: "Product #4 (made by Heidi)",
-      description: "Product #4 description",
-      imageURL: "https://picsum.photos/500/500",
-      price: 594.91,
-      stock: 0,
-      discount: false,
-      discountPct: 0,
-      isHidden: false,
-      _createdBy: user2.id,
-    },
-  ];
-
-  await productModel.insertMany(products);
+  // Generate fake product
+  for (let index = 0; index < 10; index++) {
+    await new productModel(
+      {
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        imageURL: "https://picsum.photos/500/500",
+        price: faker.commerce.price({ min: 5, max: 5000 }),
+        stock: faker.number.int({ min: 0, max: 200 }),
+        isOnDiscount: faker.datatype.boolean(0.50),
+        discountPct: faker.number.int({ min: 0, max: 100 }),
+        isHidden: false,
+        _createdBy: user1.id
+      }
+    ).save();
+  }
 
   console.log("Seeded data successfully...");
 };
