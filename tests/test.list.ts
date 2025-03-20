83b1b54d6af35ea6c3451f417092f3bf5dcd3eb5
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-03-20 13:35:44 +0100

    Comments on tests

--- tests/test.list.ts
+++ tests/test.list.ts
@@ -2,9 +2,12 @@ process.env.NODE_ENV = 'test';
 
 import { test } from "@playwright/test";
 
+// import test cases
 import health from "./health.test";
 import userTestCollection from "./user.test";
+import productTestCollection from "./product.test";
 
+// import models
 import { userModel } from "../src/models/userModel";
 import { productModel } from "../src/models/productModel";
 
@@ -14,6 +17,9 @@ import { connect, disconnect } from "../src/repository/database";
 
 dotenvFlow.config();
 
+/**
+ * Perform pre and post setup (clean-up db etc.)
+ */
 function setup() {
   //beforeEach clear test database
   test.beforeEach(async () => {
@@ -41,5 +47,9 @@ function setup() {
 
 setup();
 
-test.describe(health);
-test.describe(userTestCollection);
+// add the test collections we want to run
+test.describe(health); // simple health check test
+test.describe(userTestCollection); // valid and in-valid test
+test.describe(productTestCollection); // workflow test
+// more tests...
+
