a7f6a757656de57c63a46c09c1fd2b5c1b7a62fc
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-03-27 20:25:30 +0100

    Improved cron handler.

--- src/controllers/devController.ts
+++ src/controllers/devController.ts
@@ -7,9 +7,7 @@ import cron, { ScheduledTask } from "node-cron";
 import https from "https";
 
 // Settings
-const TOTAL_DURATION_MINUTES = 60; // Total "keep-remote-server-alive" duration 
-const MINUTES_DELTA = 5; // How often should we ping the server
-const cronPattern = "*/" + MINUTES_DELTA + " * * * *"; // Docs here: https://crontab.guru/#*/5_*_*_*_*
+const MINUTES_DELTA = 1;
 const URL = "https://ments-api-kex4.onrender.com/api/";
 let counter = 0;
 let task: ScheduledTask;
@@ -35,6 +33,17 @@ function stopPingingServer() {
 }
 
 /**
+* Stop and clear any scheduled tasks
+*/
+function cleanUpTasks() {
+  // Clean up any existing tasks
+  for (const task of cron.getTasks().values()) {
+    task.stop();
+  }
+  cron.getTasks().clear();
+}
+
+/**
  * 
  * @param req 
  * @param res 
@@ -42,16 +51,23 @@ function stopPingingServer() {
 export async function startCron(req: Request, res: Response) {
 
   try {
+    cleanUpTasks();
+
+    const cronPattern = "*/" + MINUTES_DELTA + " * * * *";
+    // Docs here: https://crontab.guru/#*/1_*_*_*_*
+    const totalDuration = parseInt(req.params.duration as string) || 60;
 
     //Initialize the task with the specified cronPattern
-    counter = TOTAL_DURATION_MINUTES;
+    counter = totalDuration; // set counter, so we can output how much time is left
     task = cron.schedule(cronPattern, pingServer, { scheduled: false });
     task.start();
 
-    setTimeout(stopPingingServer, TOTAL_DURATION_MINUTES * 60 * 1000);
+    setTimeout(stopPingingServer, totalDuration * 60 * 1000);
 
-    res.status(200).send("Started cron-job from active route call");
+    res.status(200).send("Started background task (duration:" + totalDuration + " mins)");
+
   } catch (error) {
-    res.status(500).send({ message: error });
+    console.log("Error:" + error); // Debug info
+    res.status(500).send(error);
   }
 };
